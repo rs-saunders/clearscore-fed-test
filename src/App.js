3def53c959faef61e88641e8a61dc16cfe05b2d6
Richard Saunders <richard@sdevsolutions.com>
2020-02-02 16:39:15 +0000

    test css-modules support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import styles from './App.module.css';
 
 export default class App extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.test}>
                 ClearScore FED test...
             </div>
         );
