0a57a9d6bab97e9df5c35c5a74ac7200543152c2
Richard Saunders <richard@sdevsolutions.com>
2020-02-02 16:58:24 +0000

    add css grid layout to app
    simple header main and footer with test styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import styles from './App.module.css';
 
 export default class App extends Component {
-    render() {
-        return (
-            <div className={styles.test}>
-                ClearScore FED test...
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={styles.container}>
+        <header className={styles.header}>
+          Idea Board
+        </header>
+        <main className={styles.main}>
+          Testing Testing 1, 2, 3...
+        </main>
+        <footer className={styles.footer}>
+          ClearScore FED test - Richard Saunders (Feb 2020)
+        </footer>
+      </div>
+    );
+  }
 }
